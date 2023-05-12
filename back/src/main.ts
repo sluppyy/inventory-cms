49721a54817ec5027322b9934f59a1fdb8aa6bb3
Gleb <M20XV@yandex.ru>
2023-05-12 18:51:20 +0300

    add cors

--- back/src/main.ts
+++ back/src/main.ts
@@ -8,6 +8,7 @@ import { InventoryFilter } from 'inventory/inventory.filter'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.enableCors()
   app.useGlobalPipes(new ValidationPipe())
   app.useGlobalFilters(new InventoryFilter())
 
