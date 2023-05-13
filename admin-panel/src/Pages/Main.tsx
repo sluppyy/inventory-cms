9ff3fff8a41be5817678933b0ebc9190e24abb3b
Gleb <M20XV@yandex.ru>
2023-05-13 18:50:19 +0300

    update Main

--- admin-panel/src/Pages/Main.tsx
+++ admin-panel/src/Pages/Main.tsx
@@ -1,5 +1,8 @@
 export default function Main() {
   return (
-    <div>Main</div>
+    <div className="p-4">
+      <h1>Inventory cms</h1>
+      <p>tool to manage inventory system</p>
+    </div>
   )
 }
